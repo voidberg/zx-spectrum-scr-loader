a5e5dcb58fb5c78a6b4f4b7a2a63fd86584b8bfd
Alexandru Badiu <andu@ctrlz.ro>
2017-04-25 15:12:44 +0300

    code

---
+++ brunch-config.js
@@ -0,0 +1,17 @@
+// See http://brunch.io for documentation.
+exports.files = {
+  javascripts: {
+    joinTo: {
+      'vendor.js': /^(?!app)/, // Files that are not in `app` dir.
+      'app.js': /^app/,
+    },
+  },
+  stylesheets: {joinTo: 'app.css'},
+};
+
+exports.plugins = {
+  babel: {presets: ['latest']},
+  afterBrunch: [
+    'cp app/assets/GlugGlug.scr public/'
+  ],
+};
